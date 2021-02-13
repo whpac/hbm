420b90d9a5fabe38e4baf5558b9221dc3b510eca
Marcin Szwarc <marcinszwarc@hotmail.com>
2021-02-13 16:13:30 +0100

    Adjusted to the presenter

--- src/ts/App/App.ts
+++ src/ts/App/App.ts
@@ -1,10 +1,24 @@
 import RequestDispatcher from '../Dispatcher/RequestDispatcher';
+import BrowserAdapter from '../Views/Presentation/BrowserAdapter';
+import Presenter from '../Views/Presentation/Presenter';
 import ExecutorStorage from './ExecutorStorage';
 
-let path = document.location.href.substr(document.baseURI.length);
+function Main() {
+    let path = document.location.href.substr(document.baseURI.length);
 
-let rd = new RequestDispatcher();
-rd.SetExecutorStorage(new ExecutorStorage());
-(async () => {
-    await rd.Dispatch(path);
-})();
+    let content_wrapper = document.getElementById('site-content');
+    if(content_wrapper === null) {
+        alert('Unable to retrieve the document root.');
+        return;
+    }
+    let browser_adapter = new BrowserAdapter(content_wrapper);
+    Presenter.SetDisplayAdapter(browser_adapter);
+
+    let rd = new RequestDispatcher();
+    rd.SetExecutorStorage(new ExecutorStorage());
+    (async () => {
+        await rd.Dispatch(path);
+    })();
+}
+
+Main();
