2f1dd2012f58632d6668b7981e942f22272421f0
Marcin Szwarc <marcinszwarc@hotmail.com>
2021-02-15 13:40:23 +0100

    Added an icon wrapper class

---
+++ src/ts/Views/Common/Icon.ts
@@ -0,0 +1,28 @@
+import Component from './Component';
+
+export default class Icon extends Component {
+    protected IconElement: HTMLIonIconElement;
+
+    public get IconName() {
+        return this.IconElement.name;
+    }
+    public set IconName(value: string) {
+        this.IconElement.name = value;
+    }
+
+    public constructor(icon_name: string) {
+        super();
+
+        this.IconElement = document.createElement('ion-icon') as HTMLIonIconElement;
+        this.IconElement.classList.add('icon');
+        this.IconName = icon_name;
+    }
+
+    protected Render(): HTMLElement {
+        return this.IconElement;
+    }
+}
+
+interface HTMLIonIconElement extends HTMLElement {
+    name: string;
+}
