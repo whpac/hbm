f60d719fd2917fd486d591ddfe03e3982330a67f
Marcin Szwarc <marcinszwarc@hotmail.com>
2021-02-17 16:33:45 +0100

    Converter uses the local timezone now

--- src/ts/Utils/Date.ts
+++ src/ts/Utils/Date.ts
@@ -1,6 +1,11 @@
 export default class DateTime {
 
     public static ToInputFormat(date: Date) {
-        return date.toISOString().substr(0, 16);
+        let formatted = date.getFullYear().toString().padStart(4, '0') + '-' +
+            date.getMonth().toString().padStart(2, '0') + '-' +
+            date.getDate().toString().padStart(2, '0') + 'T' +
+            date.getHours().toString().padStart(2, '0') + ':' +
+            date.getMinutes().toString().padStart(2, '0');
+        return formatted;
     }
 }
