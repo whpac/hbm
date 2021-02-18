ac833094159c141a493b01345bde558b9a23330e
Marcin Szwarc <marcinszwarc@hotmail.com>
2021-02-18 13:11:14 +0100

    Corrected handling of months

--- src/ts/Utils/Date.ts
+++ src/ts/Utils/Date.ts
@@ -2,7 +2,7 @@ export default class DateTime {
 
     public static ToInputFormat(date: Date) {
         let formatted = date.getFullYear().toString().padStart(4, '0') + '-' +
-            date.getMonth().toString().padStart(2, '0') + '-' +
+            (date.getMonth() + 1).toString().padStart(2, '0') + '-' +
             date.getDate().toString().padStart(2, '0') + 'T' +
             date.getHours().toString().padStart(2, '0') + ':' +
             date.getMinutes().toString().padStart(2, '0');
