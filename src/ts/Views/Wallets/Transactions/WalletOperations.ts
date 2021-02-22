1464f774e020dfaec0636c6f8699e9b095c32c5e
Marcin Szwarc <marcinszwarc@hotmail.com>
2021-02-22 14:42:36 +0100

    Disabled the nonimplemented buttons

--- src/ts/Views/Wallets/Transactions/WalletOperations.ts
+++ src/ts/Views/Wallets/Transactions/WalletOperations.ts
@@ -3,12 +3,17 @@ import MenuSeparator from '../../Common/MenuStrip/MenuSeparator';
 import MenuStrip from '../../Common/MenuStrip/MenuStrip';
 import SubMenu from '../../Common/MenuStrip/SubMenu';
 
-type WalletOperationsEvents = 'AddTransactionRequested' | 'EditTransactionRequested';
+type WalletOperationsEvents = 'AddTransactionRequested' | 'EditTransactionRequested' |
+    'RemoveTransactionRequested';
 
 export default class WalletOperations extends MenuStrip<WalletOperationsEvents> {
     protected AddButton: MenuButton;
     protected EditButton: MenuButton;
     protected RemoveButton: MenuButton;
+    protected FilterButton: MenuButton;
+    protected EditWalletButton: MenuButton;
+    protected RemoveWalletButton: MenuButton;
+    protected DefaultWalletButton: MenuButton;
 
     public constructor() {
         super();
@@ -18,20 +23,26 @@ export default class WalletOperations extends MenuStrip<WalletOperationsEvents> 
         this.AddMenuItem(this.EditButton = new MenuButton('Edit', { ToolTip: 'Edit the selected transaction', IconName: 'create-outline' }));
         this.AddMenuItem(this.RemoveButton = new MenuButton('Remove', { ToolTip: 'Remove the selected transaction', IconName: 'trash-outline' }));
 
-        this.AddSecondaryMenuItem(new MenuButton('Filter', { ToolTip: 'Decide which transactions to display', IconName: 'filter-outline' }));
+        this.AddSecondaryMenuItem(this.FilterButton = new MenuButton('Filter', { ToolTip: 'Decide which transactions to display', IconName: 'filter-outline' }));
         this.AddSecondaryMenuItem(new MenuSeparator());
 
         let wallet_submenu = new SubMenu(new MenuButton('Wallet'));
-        wallet_submenu.AddMenuItem(new MenuButton('Edit', { IconName: 'create-outline' }));
-        wallet_submenu.AddMenuItem(new MenuButton('Remove', { IconName: 'trash-outline' }));
+        wallet_submenu.AddMenuItem(this.EditWalletButton = new MenuButton('Edit', { IconName: 'create-outline' }));
+        wallet_submenu.AddMenuItem(this.RemoveWalletButton = new MenuButton('Remove', { IconName: 'trash-outline' }));
         wallet_submenu.AddMenuItem(new MenuSeparator());
-        wallet_submenu.AddMenuItem(new MenuButton('Set as default'));
+        wallet_submenu.AddMenuItem(this.DefaultWalletButton = new MenuButton('Set as default'));
         this.AddSecondaryMenuItem(wallet_submenu);
 
         this.SetIsAnyTransactionSelected(false);
 
         this.AddButton.AddEventListener('Click', (() => this.FireEvent('AddTransactionRequested')).bind(this));
         this.EditButton.AddEventListener('Click', (() => this.FireEvent('EditTransactionRequested')).bind(this));
+        this.RemoveButton.AddEventListener('Click', (() => this.FireEvent('RemoveTransactionRequested')).bind(this));
+
+        this.FilterButton.Enabled = false;
+        this.EditWalletButton.Enabled = false;
+        this.RemoveWalletButton.Enabled = false;
+        this.DefaultWalletButton.Enabled = false;
     }
 
     /**
